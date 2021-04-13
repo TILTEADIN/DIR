fcf109f034a8dd0ee3739875b1aa64c3b984806c
Eduardo Eiroa <61843947+TILTEADIN@users.noreply.github.com>
2021-04-13 19:44:52 +0200

    Update hipercubo.c

--- hipercubo.c
+++ hipercubo.c
@@ -19,7 +19,7 @@ float maximum(int rank,int neighbours[],float node_buffer);
 
 int main(int argc, char *argv[]){
 
-    int rank, size; //Definimos las variables rank y size
+    int rank, size; 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
